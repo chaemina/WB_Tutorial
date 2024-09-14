1df5f2a428d5a68b89d6a5724a0be6592a4ad9f4
chaemina <chaemina82@naver.com>
2024-09-15 01:19:27 +0900

    refactoring

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -7,27 +7,26 @@ export default function Home() {
         <p className="text-white font-bold text-[30px]">Waste Bank APP</p>
       </header>
       <div className="grid grid-rows-[auto_auto_auto] items-start justify-center font-[family-name:var(--font-geist-sans)] gap-8 p-10"> 
-      <main className="gap-4 flex flex-col row-start-1 items-center sm:items-start">
-          <p className="font-bold text-lime-600 text-[25px]">Application Download</p>
+      <main className="gap-4 flex flex-col row-start-1 items-center  justify-center">
+          <p className="font-bold text-lime-600 text-[25px] md:text-[30px] ">Application Download</p>
           <p className="text-[17px]">Anda dapat menginstal file apk aplikasi.</p>
           <div className="w-full items-center justify-center flex flex-col">
-          <div className="bg-lime-600 p-4 rounded-xl text-white">
+          <div className="bg-lime-600 p-4 rounded-xl text-white md:text-[30px]">
           <a href="/WasteBankApp.zip" download>
-            Download Application
+            Download
             </a>
           </div>
           </div>
         </main>
-         <main className="gap-4 flex flex-col row-start-2 items-center sm:items-start">
-          <p className="font-bold text-lime-600 text-[25px]">Tutorial Video</p>
+         <main className="gap-4 flex flex-col row-start-2 items-center  justify-center">
+          <p className="font-bold text-lime-600 text-[25px] md:text-[30px]">Tutorial Video</p>
           <p className="text-[17px]">Bagaimana saya bisa menggunakan aplikasi ini?</p>
-          <div className="w-full flex items-center justify-center">
+          <div className="w-full md:w-[700px] flex items-center justify-center">
             <iframe
-              width="560"
-              height="315"
+              width="90%"
+              height="300px"
               src="https://www.youtube.com/embed/ffF9OGOU7h4" 
               title="YouTube video player"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
               allowFullScreen
             ></iframe>
@@ -35,14 +34,6 @@ export default function Home() {
         </main>
        
         <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-            <Image
-              aria-hidden
-              src="https://nextjs.org/icons/file.svg"
-              alt="File icon"
-              width={16}
-              height={16}
-            />
-            chaemina82@naver.com
           <Image
             aria-hidden
             src="https://nextjs.org/icons/globe.svg"
@@ -50,7 +41,8 @@ export default function Home() {
             width={16}
             height={16}
           />
-          From Team Holssi
+          From Holssi of KIV
+        
         </footer>
       </div>
     </>
